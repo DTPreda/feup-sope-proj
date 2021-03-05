8415259a09827412a789f354b599306ac9f8bf93
David Preda <dtpreda30@gmail.com>
2021-03-05 22:09:41 +0000

    Takes some of main away from it to another function. Starts recursive version for a directory and its subdirectories

--- proj-1/xmod.c
+++ proj-1/xmod.c
@@ -12,7 +12,7 @@ __mode_t parse_perms(char* perms, char* filename){
     char target = 'a';
     char mode;
     unsigned int read = 0, write = 0, execute = 0;
-    for(int i = len - 2; perms[i] != '+' && perms[i] != '-' && perms[i] != '=' && i > 0; i--){
+    for(int i = len - 1; perms[i] != '+' && perms[i] != '-' && perms[i] != '=' && i > 0; i--){
         if(perms[i] == 'r') read = 1;
         if(perms[i] == 'w') write = 1;
         if(perms[i] == 'x') execute = 1;
@@ -22,6 +22,7 @@ __mode_t parse_perms(char* perms, char* filename){
 	 printf("Ganda murcao puseste isso mal ze\n");
 	 exit(1);
     }
+
     switch (perms[0]){
         case 'u':
             target = 'u';
@@ -94,6 +95,55 @@ __mode_t get_perms(unsigned int r, unsigned int w, unsigned int x, char op, char
     return ret;
 }
 
+void chmod_dir(char* cmd, char* dir_name){
+    //filename points to a dir
+    char filename[100];
+    DIR* d;
+    struct dirent *dir;
+    d = opendir(dir_name);
+    if(d) {
+        while((dir = readdir(d)) != NULL){
+            if(dir->d_type == DT_REG && dir->d_name[0] != '.') { //if it is a regular file
+                strcpy(filename, "./");
+                strcat(filename, dir_name); strcat(filename, "/"); // filename = dir_name/
+
+                strcat(filename, dir->d_name);
+                __mode_t mode = parse_perms(cmd, filename);
+
+                printf("File to be inspected: %s\n", filename);
+                if(chmod(filename, mode) != 0){
+                    perror("chmod");
+                }
+            } else if (dir->d_type == DT_DIR && (dir->d_name)[0] != '.'){
+                int pid;
+                if((pid = fork()) < 0){
+                    perror("fork");
+                    return;
+                }
+                if(pid == 0){
+                    printf("Hello there from the child\n");
+                    strcpy(filename, ""); 
+                    strcat(filename, dir_name); strcat(filename, "/");
+                    strcat(filename, dir->d_name);
+                    printf("New dir to be inspected: %s\n", filename);
+                    chmod_dir(cmd, filename);
+                } else {
+                    printf("Hello there from the parent\n");
+                    wait(0);
+                }
+            }
+        }
+
+        __mode_t mode = parse_perms(cmd, dir_name);
+        //printf("Im the directory: %s", argv[2]);
+        if(chmod(dir_name, mode) != 0){
+            perror("chmod");
+            exit(1);
+        }
+    }
+
+}
+
 int main(int argc, char* argv[]){
     if(argc <= 2){
         fprintf(stdout, "Invalid number of arguments");
@@ -108,34 +158,8 @@ int main(int argc, char* argv[]){
 
     __mode_t arg_info = st.st_mode;
     if((arg_info & __S_IFDIR) != 0){
-	//printf("Im the directory: %s", argv[2]);
+        chmod_dir(argv[1], argv[2]);
 
-        //filename points to a dir
-        char filename[100]; strcpy(filename, "./");
-        DIR* d;
-        struct dirent *dir;
-        d = opendir(argv[2]);
-        if(d) {
-            while((dir = readdir(d)) != NULL){
-                if(dir->d_type == DT_REG) { //if it is a regular file
-                    strcpy(filename, "./");
-                    strcat(filename, argv[2]); strcat(filename, "/"); // filename = dir_name/
-
-                    strcat(filename, dir->d_name);
-                    __mode_t mode = parse_perms(argv[1], filename);
-
-                    if(chmod(filename, mode) != 0){
-                        perror("chmod");
-                    }
-                }
-            }
-	    __mode_t mode = parse_perms(argv[1], argv[2]);
-            //printf("Im the directory: %s", argv[2]);
-            if(chmod(argv[2], mode) != 0){
-            	perror("chmod");
-            	exit(1);
-            }
-        }
     } else {
         //filename points to a file
         __mode_t mode = parse_perms(argv[1], argv[2]);
