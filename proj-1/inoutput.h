e07e2f16537d04caba3eb9373887f9f99014aa89
Krypt0 <tiagopbr.2001@gmail.com>
2021-03-17 19:52:48 +0000

    Ok now the changes are actually pushed

--- proj-1/inoutput.h
+++ proj-1/inoutput.h
@@ -5,7 +5,7 @@ int parse_argv(int argc, char* argv[]);
 int parse_perm_arg_octal(char* arg);
 void format_argv(int argc, char *argv[], char* str);
 int parse_perm_arg(char* arg);
-char * format_octal(char *octal);
+void format_octal(char *octal, char* in);
 int get_options(int* verbose, int* recursive, int* index, int argc, char* argv[]);
 void get_input(char* input, char* in, char* file_name, int index, int argc, char* argv[]);
 void print_changes(__mode_t new_mode, __mode_t old_mode, int verbosity, char* file_name);
