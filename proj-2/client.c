56b6e04852b69e160d605828041144ed6e52b95a
ruialves35 <ruialves20010323@gmail.com>
2021-04-10 12:29:27 -0400

    Deleted semaphore to control Private Fifo because we dont need it

--- proj-2/client.c
+++ proj-2/client.c
@@ -2,17 +2,15 @@
 #include<fcntl.h>
 #include <time.h>
 #include <pthread.h>
-#include <semaphore.h>
 
 char* public_pipe;
 static int global_id = 0;
 pthread_mutex_t access_public_pipe;
-sem_t access_private_pipe;
 
 
 /**
  * Make a request to Servidor
- * Probably need to pass the struct request throw argument and store it in public_pipe 
+ * Probably need to pass the struct request throw argument and store it in public_pipe (putting it in public_pipe is making a request)
  */ 
 void make_request() {
     pthread_mutex_lock(&access_public_pipe);        // Just one thread accessing the public pipe to register requests
@@ -30,14 +28,14 @@ void make_request() {
 
 /**
  * Get response from a request. 
- * Probably need to return a struct request, read from the private pipe
+ * Probably need to return a struct request, read from the private pipe (idk how to read it from pipe, how does the output of the request come?)
  */ 
 void get_response() {
-    sem_wait(&access_private_pipe);
     char* private_pipe;
     char receivedMessage[1024];
     int np;
     // Writing the task Client want Servidor to perform
+
     np = open(public_pipe, O_RDONLY);
     if (np < 0) {
         fprintf(stdout, "Could not open public_pipe\n");
@@ -46,13 +44,14 @@ void get_response() {
     }
 
     /* PROCESS MESSAGE, WRITE THAT RECEIVED THE RETURN OF THE REQUEST, ETC */
-    sem_post(&access_private_pipe);
 }
 
-void *client_func(void * argument) {
+/**
+ * Func to create client threads. It creates a private fifo to communicate with Servidor
+ */
+void *client_thread_func(void * argument) {
     client_order order;
     // the client can access the semaphore and the Servidor to
-    sem_init(&access_private_pipe, 0, 2);
 
     // generate number between 1 and 9
     int upper = 9, lower = 1;
@@ -60,9 +59,9 @@ void *client_func(void * argument) {
     order.id = global_id++;
     order.priority = num;
 
-    char* private_pipe;
-    snprintf(private_pipe, "/tmp/%d.%lu", gepid(), (unsigned long) pthread_self());
-    if (mkfifo(private_pipe, 0666) < 0) return 1;     // public channel
+    char* private_fifo;
+    snprintf(private_fifo, "/tmp/%d.%lu", gepid(), (unsigned long) pthread_self());
+    if (mkfifo(private_fifo, 0666) < 0) return 1;     // public channel
 
     make_request();
 }
