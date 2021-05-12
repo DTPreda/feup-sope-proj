ffe5404643ecabc39597e65adcbc068264f6ea6a
Krypt0 <tiagopbr.2001@gmail.com>
2021-05-12 11:31:43 +0100

    More linting changes to the server

--- proj-2/src_v2/server.c
+++ proj-2/src_v2/server.c
@@ -1,4 +1,4 @@
-#include "server.h"
+#include "headers/server.h"
 
 
 int input_time;
@@ -17,10 +17,10 @@ sem_t empty;
 sem_t full;
 sem_t curr_n_threads;
 
-int get_remaining_time(){
+int get_remaining_time() {
     time_t current_time = time(NULL);
     time_t ret = input_time - current_time + start_time;
-    if(ret < 0) return 0;
+    if (ret < 0) return 0;
     return ret;
 }
 
@@ -53,13 +53,13 @@ void register_operation(Message* msg, int type) {
     pthread_mutex_unlock(&output_mutex);
 }
 
-int parse_arguments(int argc, char* argv[]){
+int parse_arguments(int argc, char* argv[]) {
     if (argc != 4 && argc != 6) return 1;
     if (argc == 6) {
-        if(strncmp(argv[3], "-l", 2) != 0) return 1;
+        if (strncmp(argv[3], "-l", 2) != 0) return 1;
 
         buffer_size = atoi(argv[4]);
-        if(buffer_size == 0) return 1;
+        if (buffer_size == 0) return 1;
 
         public_fifo = argv[5];
     } else {
@@ -67,9 +67,9 @@ int parse_arguments(int argc, char* argv[]){
     }
 
     if (strncmp(argv[1], "-t", 2) != 0) return 1;
-    
+
     input_time = atoi(argv[2]);
-    if(input_time == 0) return 1;
+    if (input_time == 0) return 1;
     return 0;
 }
 
@@ -77,8 +77,8 @@ int set_server_up(message_queue* q, int bufsz, char* fifo_name) {
     if (queue_init(q, buffer_size) != 0) {
         fprintf(stderr, "queue_init: Unable to initialize queue.\n");
         return 1;
-    } 
-    
+    }
+
     if (mkfifo(fifo_name, 0666) != 0) {
         perror("mkfifo");
         return 1;
@@ -107,15 +107,15 @@ int get_request(Message* msg) {
     tv.tv_usec = 0;
 
     int sl;
-    if((sl = select(public_fifo_fd + 1, &rfds, NULL, NULL, &tv)) == -1) {
+    if ((sl = select(public_fifo_fd + 1, &rfds, NULL, NULL, &tv)) == -1) {
         perror("select");
         return 1;
     } else if (sl > 0) {
         int r = read(public_fifo_fd, msg, sizeof(Message));
-        if(r == -1) {
+        if (r == -1) {
             perror("read");
             return 1;
-        } else if (r == 0){
+        } else if (r == 0) {
             return 2;
         }
         register_operation(msg, RECVD);
@@ -126,7 +126,7 @@ int get_request(Message* msg) {
     return 0;
 }
 
-int get_request_non_timeout(Message* msg){
+int get_request_non_timeout(Message* msg) {
     int r = read(public_fifo_fd, msg, sizeof(Message));
     if (r == -1) {
         return 1;
@@ -146,7 +146,7 @@ void free_resources() {
 
     pthread_mutex_destroy(&output_mutex);
     pthread_mutex_destroy(&queue_access);
-    
+
     queue_destroy(&buffer);
 }
 
@@ -154,13 +154,15 @@ void close_fifo(pthread_t* tid, pthread_attr_t* attr) {
     if (unlink(public_fifo) != 0) {
         perror("unlink");
     }
-    
+
     while (1) {
         Message* msg = (Message*) malloc(sizeof(Message));
         int r;
-        if((r = get_request_non_timeout(msg)) == 2) {
+        if ((r = get_request_non_timeout(msg)) == 2) {
             break;
-        } else if (r != 0) continue;
+        } else if (r != 0) {
+           continue;
+        }
 
         pthread_attr_init(attr);
         pthread_attr_setdetachstate(attr, PTHREAD_CREATE_DETACHED);
@@ -189,7 +191,7 @@ void insert_item(Message* msg, message_queue* buffer) {
 void *attend_request(void* argument) {
     sem_post(&curr_n_threads);
     Message* msg = (Message*) argument;
-    
+
     if (get_remaining_time() != 0) {
         msg->tskres = task(msg->tskload);
         register_operation(msg, TSKEX);
@@ -241,17 +243,14 @@ void *consumer_thread(void* argument) {
         }
 
         if (msg.tskres == POISON_PILL) {
-            //printf("Found poison pill\n");
             break;
         }
-        
+
         send_result(&msg);
     }
     int val;
     sem_getvalue(&curr_n_threads, &val);
     while (!queue_is_empty(&buffer) || val != 0) {
-
-
         if (sem_wait(&full) != 0) {
             continue;
         }
@@ -263,9 +262,9 @@ void *consumer_thread(void* argument) {
         if (sem_post(&empty) != 0) {
             perror("sem_post");
         }
-        
+
         send_result(&msg);
-        
+
         sem_getvalue(&curr_n_threads, &val);
     }
     fprintf(stdout, "Consumer thread terminating.\n");
@@ -292,12 +291,11 @@ void poison_pill() {
     pthread_mutex_lock(&queue_access);
     queue_push(&buffer, msg);
     pthread_mutex_unlock(&queue_access);
-    
+
     sem_post(&full);
 }
 
 int main(int argc, char* argv[]) {
-
     if (parse_arguments(argc, argv) != 0) {
         fprintf(stdout, "Correct usage: s <-t nsecs> [-l bufsz] <fifoname>\n");
         return 1;
@@ -337,10 +335,8 @@ int main(int argc, char* argv[]) {
     atexit(free_resources);
 
     fprintf(stdout, "Reader thread terminating.\n");
-    
+
     poison_pill();
 
     pthread_exit(NULL);
-
-    //return 0;
 }
