e70cb0b160cde6df51d600caa23907be2507bb92
David Preda <dtpreda30@gmail.com>
2021-05-31 23:07:04 +0100

    Rearranjes repo

--- proj-2/servidor_testar/headers/lib.h
+++
@@ -1,4 +0,0 @@
-#ifndef HEADERS_LIB_LIB_H_
-#define HEADERS_LIB_LIB_H_ 1
-int task(int level);
-#endif  // HEADERS_LIB_LIB_H_
--- proj-2/src/delay.h
+++
@@ -1,4 +0,0 @@
-#ifndef  SRC_DELAY_H_
-#define  SRC_DELAY_H_ 1
-extern int delay;
-#endif  // SRC_DELAY_H_
